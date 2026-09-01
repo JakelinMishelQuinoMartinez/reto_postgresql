bb8046b919cda4ba7ec44ad3964b7a7eb27a7f7f
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-09-01 13:56:53 -0600

    fix(ddl-dql): ubicar scripts en archivos correspondientes

--- database/ddl/schema.sql
+++ database/ddl/schema.sql
@@ -13,3 +13,10 @@ CREATE TABLE  juegos (
     fecha_lanzamiento DATE,
     categoria_id INT NOT NULL REFERENCES categorias(categoria_id)
 );
+
+CREATE TEMP TABLE temporal_json(data JSONB);
+\COPY temporal_json(data) FROM PROGRAM 'tr -d "\r\n" < /home/camper/postgresdata/categorias.json';
+
+
+CREATE TEMP TABLE temporal_xml (data XML);
+\copy temporal_xml(data) FROM PROGRAM 'tr -d "\r\n" < /home/camper/postgresdata/juegos.xml'
