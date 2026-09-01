a839ab9acff51061bb656e652f8c2d869bfdc4bb
steff-360 <steff.18random@gmail.com>
2026-09-01 13:04:45 -0600

    feat(dql):importar xml

--- database/dql/queries.sql
+++ database/dql/queries.sql
@@ -10,3 +10,27 @@ SELECT
      e->>'descripcion'
 FROM temporal_json AS t 
 CROSS JOIN LATERAL  jsonb_array_elements(t.data) AS e;
+
+CREATE TEMP TABLE temporal_xml (data XML);
+
+\copy temporal_xml(data) FROM PROGRAM 'tr -d "\r\n" < /home/camper/postgresdata/juegos.xml'
+
+INSERT INTO juegos (
+    codigo, titulo, precio, 
+    fecha_lanzamiento, categoria_id
+)
+SELECT x.codigo, x.titulo, 
+       x.precio::numeric, 
+       x.fecha_lanzamiento::date, 
+       x.categoria_id::int
+FROM temporal_xml AS t,
+XMLTABLE(
+    '/juegos/juego'
+    PASSING t.data
+    COLUMNS 
+        codigo TEXT PATH 'codigo',
+        titulo TEXT PATH 'titulo',
+        precio TEXT PATH 'precio',
+        fecha_lanzamiento TEXT PATH 'fecha_lanzamiento',
+        categoria_id TEXT PATH 'categoria_id'
+) AS x;
